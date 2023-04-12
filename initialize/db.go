0e2a0c61385423512dc3df63eccf306afe87ffd1
Amit <75199676+amit921999@users.noreply.github.com>
2023-04-12 12:42:03 +0530

    Add files via upload

--- initialize/db.go
+++ initialize/db.go
@@ -7,9 +7,9 @@ import (
 
 var DB *gorm.DB
 
+var dsn = "root:password@tcp(127.0.0.1:3306)/sellerapp?charset=utf8mb4&parseTime=True&loc=Local"
+
 func ConnectDB() {
-	// refer https://github.com/go-sql-driver/mysql#dsn-data-source-name for details
-	dsn := "root:password@tcp(127.0.0.1:3306)/sellerapp?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
--- initialize/migration.go
+++ initialize/migration.go
@@ -7,13 +7,22 @@ import (
 )
 
 func Migrate() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/sellerapp?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
 
 	err = db.AutoMigrate(&models.Order{}, &models.Item{})
+	if err != nil {
+		panic(err)
+	}
+
+	sqlConn, err := db.DB()
+	if err != nil {
+		panic(err)
+	}
+
+	err = sqlConn.Close()
 	if err != nil {
 		panic(err)
 	}
